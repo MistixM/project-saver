9da4dc83daab1fb5be576d85925ab9060230a127
MDEV <108402379+MistixM@users.noreply.github.com>
2024-12-30 18:59:11 +0200

    Update test.py

--- test.py
+++ test.py
@@ -8,10 +8,10 @@ def main():
     print(a + b)
 
 if __name__ == "__main__":
-    url = 'https://mistixm.pythonanywhere.com/request_access'
+    url = 'https://some-cool-website-host.com/request_access'
     request = requests.post(url, json={"api": "gfgfgs"}).status_code
 
     if not request == 200:
-        sys.exit("Not allowed. Contact with mdev: https://mdev.uk.to")
+        sys.exit("Not allowed.")
 
     main()
